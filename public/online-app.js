598cf024bb85d43fd701af91ba30f5476490c5a5
AJ Collins <16407457+ajc133@users.noreply.github.com>
2024-05-28 18:08:43 -0700

    async / await fetch

--- public/online-app.js
+++ public/online-app.js
@@ -1,33 +1,26 @@
 const pastExpenseContainer = document.getElementById("expenses");
 
-function renderPastExpenses() {
-  const expenseQuery = new Request("/expenses");
-  fetch(expenseQuery)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status ${response.status}`);
-      }
-      return response.json();
+async function renderPastExpenses() {
+  try {
+    const response = await fetch("/expenses");
+    const data = await response.json();
+    pastExpenseContainer.innerHTML = "";
+
+    const pastExpenseHeader = document.createElement("h2");
+    pastExpenseHeader.textContent = "Past Expenses";
+
+    const pastExpenseList = document.createElement("h1");
+    data.expenses.forEach((expense) => {
+      const expenseEl = document.createElement("li");
+      expenseEl.textContent = `User ${expense.userId} paid \$${expense.cost} for ${expense.item}`;
+      pastExpenseList.appendChild(expenseEl);
     })
-    .then((response) => {
-      pastExpenseContainer.innerHTML = "";
 
-      const pastExpenseHeader = document.createElement("h2");
-      pastExpenseHeader.textContent = "Past Expenses";
-
-      const pastExpenseList = document.createElement("h1");
-      response.expenses.forEach((expense) => {
-        const expenseEl = document.createElement("li");
-        expenseEl.textContent = `User ${expense.userId} paid \$${expense.cost} for ${expense.item}`;
-        pastExpenseList.appendChild(expenseEl);
-      })
-
-      pastExpenseContainer.appendChild(pastExpenseHeader);
-      pastExpenseContainer.appendChild(pastExpenseList);
-    })
-  // if (expenses.length === 0) {
-  //   return;
-  // }
-
+    pastExpenseContainer.appendChild(pastExpenseHeader);
+    pastExpenseContainer.appendChild(pastExpenseList);
+  } catch (error) {
+    console.log(error);
+  }
 }
+
 renderPastExpenses();
