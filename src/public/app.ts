0d8fe7e8a9f12d9145e988de610390ad6c3a225a
AJ Collins <16407457+ajc133@users.noreply.github.com>
2024-05-29 18:53:31 -0700

    render users

--- src/public/app.ts
+++ src/public/app.ts
@@ -1,37 +1,74 @@
 import { Expense, User } from "@prisma/client"
 
-const pastExpenseContainer = document.getElementById("expenses")
+const expenseContainer = document.getElementById("expenses")
+const usersContainer = document.getElementById("users")
 
-async function getUsers() {
+async function getUsers(): Promise<User[]> {
   try {
     const response = await fetch("/users")
+    const users: User[] = await response.json()
+    return users
   }
   catch (error) {
-
+    console.log(error)
   }
-
 }
-async function renderPastExpenses() {
+
+async function getExpenses(): Promise<Expense[]> {
   try {
     const response = await fetch("/expenses")
-    const data = await response.json()
-    pastExpenseContainer.innerHTML = ""
+    const expenses: Expense[] = await response.json()
+    return expenses
+  }
+  catch (error) {
+    console.log(error)
+  }
+}
 
-    const pastExpenseHeader = document.createElement("h2")
-    pastExpenseHeader.textContent = "Past Expenses"
 
-    const pastExpenseList = document.createElement("h1")
-    data.expenses.forEach((expense: Expense) => {
+async function renderExpenses() {
+  try {
+    expenseContainer.innerHTML = ""
+
+    const expenseHeader = document.createElement("h1")
+    expenseHeader.textContent = "Past Expenses"
+    const expenseList = document.createElement("ul")
+
+    const expenses = await getExpenses()
+    expenses.forEach((expense) => {
       const expenseEl = document.createElement("li")
       expenseEl.textContent = `User ${expense.userId} paid \$${expense.cost.toFixed(2)} for ${expense.item}`
-      pastExpenseList.appendChild(expenseEl)
+      expenseList.appendChild(expenseEl)
     })
 
-    pastExpenseContainer.appendChild(pastExpenseHeader)
-    pastExpenseContainer.appendChild(pastExpenseList)
+    expenseContainer.appendChild(expenseHeader)
+    expenseContainer.appendChild(expenseList)
   } catch (error) {
     console.log(error)
   }
 }
 
-renderPastExpenses()
+async function renderUsers() {
+  try {
+    usersContainer.innerHTML = ""
+
+    const usersHeader = document.createElement("h1")
+    usersHeader.textContent = "Users"
+    const usersList = document.createElement("ul")
+
+    const users = await getUsers()
+    users.forEach((user) => {
+      const userEl = document.createElement("li")
+      userEl.textContent = `Username: ${user.name}\tEmail: ${user.email}`
+      usersList.appendChild(userEl)
+    })
+
+    usersContainer.appendChild(usersHeader)
+    usersContainer.appendChild(usersList)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+renderExpenses()
+renderUsers()
